ee8552b3ce43d33d8a1c814ab8d0a0975f6a36aa
ask0908 <59863631+ask0908@users.noreply.github.com>
2022-04-24 23:55:49 +0900

    Time: 16 ms (31.33%), Space: 45.9 MB (15.78%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,9 +3,8 @@ class Solution {
         if (x < 0) return false;
         String intStr = Integer.valueOf(x).toString();
         int strLen = intStr.length();
-        for (int i = 0; i < strLen / 2; i++) {
+        for (int i = 0; i < strLen / 2; i++)
             if (intStr.charAt(i) != intStr.charAt(intStr.length() - 1 - i)) return false;
-        }
         return true;
     }
 }
